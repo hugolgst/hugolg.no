3315a3426d8858c82a6a30467fac4684474a7c86
Hugo Lageneste <hugo@testnor.com>
2025-03-25 15:34:44 +0100

    Uncomment linear blur

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -3,19 +3,14 @@
 import { Flex } from '@chakra-ui/react'
 import { PropsWithChildren } from 'react'
 
-// import LinearBlur from '@/components/miscellaneous/LinearBlur'
+import LinearBlur from '@/components/miscellaneous/LinearBlur'
 import NavigationBar from '@/components/NavigationBar'
 
 const Layout = ({ children }: PropsWithChildren) => (
   <Flex pos='relative'>
     {children}
 
-    {/* <LinearBlur */}
-    {/*   height='350px' */}
-    {/*   strength={128} */}
-    {/*   steps={16} */}
-    {/*   tint='rgba(255, 255, 255, 0.8)' */}
-    {/* /> */}
+    <LinearBlur height='300px' />
     <NavigationBar />
   </Flex>
 )
